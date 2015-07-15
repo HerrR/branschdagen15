5a8071270ca6ced9dd9c706925793a92dc51030e
HerrR <ribe93@live.se>
2015-07-15 23:07:26 +0200

    Styling started
    Menu can open and close
    Colors in place
    Fancy link color animation on hover
    Needs mobile adjusting

--- js/appCtrl.js
+++ js/appCtrl.js
@@ -1,3 +1,18 @@
 app.controller('AppCtrl', function ($scope, Model) {
-	
+	var activeMenu = false;
+	$scope.toggleMenu = function(){
+		console.log("Menu toggled!");
+		if(activeMenu){
+			console.log("closing menu");
+			$("#menu").removeClass("menuOpen");
+			$("#menu").addClass("menuClosed");
+
+			activeMenu = false;
+		} else {
+			console.log("opening menu");
+			$("#menu").removeClass("menuClosed");
+			$("#menu").addClass("menuOpen");
+			activeMenu = true;
+		}
+	}
 })
